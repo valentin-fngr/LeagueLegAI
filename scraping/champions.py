f8060326bcb0ddde5b199fd400410f2efa1ce842
valentin <valentin.fontanger@gmail.com>
2021-04-02 14:54:15 +0200

    Collected champions and their id, serializer

---
+++ scraping/champions.py
@@ -0,0 +1,35 @@
+import pickle
+import requests 
+import os
+
+URL = "http://ddragon.leagueoflegends.com/cdn/11.7.1/data/en_US/champion.json"
+API_KEY = os.environ.get("RIOT_KEY")
+headers = {
+    "X-Riot-Token": API_KEY
+}
+
+
+def main(): 
+    champion_dict = {}
+    
+    try: 
+        r = requests.get(URL, headers=headers)
+        content = r.json()
+        data = content["data"]
+        for champion in data:   
+            champion_dict[data[champion]["key"]] = data[champion]["name"]
+
+        # serialize the list 
+        with open("champion_list", "wb") as f: 
+            pickle.dump({"champion_list" : champion_dict}, f)
+            print("Successfully serialized the champion_list ! ")
+            print(f"Total champs : {len(champion_dict)}")
+        
+        print(champion_dict)
+    except Exception as e: 
+        raise e
+        
+        
+
+if __name__ == "__main__":
+    main()
--- scraping/match.py
+++ scraping/match.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import datetime.datetime
 # GET USER ID ! https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/oraxan{summonerName} | accountId
 # GET MATCH BY USER : GET /lol/match/v4/matchlists/by-account/{encryptedAccountId}
 # GET MATCH DETAILS : GET /lol/match/v4/matches/{matchId}
@@ -25,7 +26,6 @@ headers = {
     "X-Riot-Token": API_KEY
 }
 
-
 def fetch_user_account_id(summoner_name): 
     '''
         fetch a user account's details
@@ -45,10 +45,10 @@ def fetch_user_account_id(summoner_name):
 
 
 def fetch_user_matches(account_id, endIndex=30): 
-    print("ok")
     '''
         fetch user's latest 30 matches
     '''
+    print(account_id)
     url = MATCH_BY_USER_URL + account_id
     matches = []
     params = {"endIndex" : endIndex}
@@ -62,5 +62,110 @@ def fetch_user_matches(account_id, endIndex=30):
     finally: 
         return matches
 
+
+def fetch_match_details(match_id): 
+    '''
+        return a single match details response's body
+    '''
+    url = MATCH_DETAILS_URL + match_id
+    game_detail = {}
+    try: 
+        r = requests.get(url, headers=headers)
+        game_detail = r.json()
+        print(content)
+
+    except Exception as e: 
+        raise e    
+    finally: 
+        return game_detail
+        
+
+
+
+class PlayerSerializer:
+
+    def __init__(self, champion, rank): 
+        self.champion = champion
+        self.rank = rank
+
+class TeamSerializer: 
+
+    def __init__(self): 
+        self.player1 = None # champ id and rank
+        self.player2 = None 
+        self.player3 = None 
+        self.player4 = None 
+        self.player5 = None 
+        self.game_status = None # Win or Fail
+        self.team_id = None
+        self.firstBlood = None
+        self.firstBlood = None 
+        self.firstTower = None 
+        self.firstBaron = None 
+        self.firstDragon = None 
+        self.towerKills = None
+    
+        
+
+    @classmethod
+    def from_dict(cls, team_response):
+        team_data = {
+            "teamId" : team_response["teamId"]
+            "status" : team_response["win"], 
+            "firstBlood" : team_response["firstBlood"],
+            "firstTower" : team_response["firstTower"], 
+            "firstBaron" : team_response["firstBaron"], 
+            "firstDragon" : team_response["firstDragon"], 
+            "towerKills" : team_response["towerKills"]
+        }
+
+        
+
+
+class MatchSerializer: 
+    
+    def __init__(self, gameId, plateformId, gameDuration, seasonId, gameMode, teamOne, teamTwo, winner): 
+        self.gameId = gameId 
+        self.plateformId = plateformId 
+        self.gameDuration = gameDuration
+        self.seasonId = seasonId
+        self.gameMode = gameMode
+        # list of players in team 1
+        self.teamOne = teamOne 
+        # list of players in team 2 
+        self.teamTwo = teamTwo
+        self.winner = winner
+
+
+    @classmethod 
+    def from_response_body(cls, match_body): 
+        '''
+            return a MatchSerializer Instance with both team's informations set
+            Arguments : 
+                match_body : match_body from response
+        '''
+        match_gen_details = {
+            "gameId" : match_body["gameId"], 
+            "plateformeId" : match_body["plateformId"], 
+            "gameDuration" : match_body["gameDuration"], 
+            "seasonId" : match_body["seasonId"], 
+            "gameMode" : match_body["gameMode"]
+        }
+        
+        match_instance = cls(**match_gen_details)
+
+        # build teams data 
+        tea
+        
+
+
+    def to_csv(self, file_path): 
+        # todo : format the instance as a csv
+        pass
+
+
 user_id = fetch_user_account_id("oraxan")
-fetch_user_matches(user_id)
+matches = fetch_user_matches(user_id)
+match_id = str(matches[0]["gameId"])
+print(match_id)
+fetch_match_details(match_id)
