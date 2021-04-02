a89ddf5c4b7d7087048ae311cd0fc528e29eed24
unknown <valentin.fontanger@gmail.com>
2021-04-02 21:39:57 +0200

    Match, player and Team serializer

--- scraping/match.py
+++ scraping/match.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import json
-import datetime.datetime
+import datetime
 # GET USER ID ! https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/oraxan{summonerName} | accountId
 # GET MATCH BY USER : GET /lol/match/v4/matchlists/by-account/{encryptedAccountId}
 # GET MATCH DETAILS : GET /lol/match/v4/matches/{matchId}
@@ -20,7 +20,7 @@ API_KEY = os.environ.get("RIOT_KEY")
 USER_DETAIL_URL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
 MATCH_BY_USER_URL = "https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/"
 MATCH_DETAILS_URL = "https://euw1.api.riotgames.com/lol/match/v4/matches/"
-CHAMP_DETAIL_URL = "http://ddragon.leagueoflegends.com/cdn/11.7.1/data/en_US/champion/" # + champ.json
+
 
 headers = {
     "X-Riot-Token": API_KEY
@@ -81,17 +81,49 @@ def fetch_match_details(match_id):
         
 
 
-
+# DONE 
 class PlayerSerializer:
 
-    def __init__(self, champion, rank): 
-        self.champion = champion
-        self.rank = rank
+    def __init__(self): 
+        self.championId = None
+        self.championName = None
+        self.kills = None 
+        self.deaths = None 
+        self.assists = None 
+        self.totalDamageDealt = None 
+        self.visionScore = None
+        self.totalDamageTaken = None
+        self.turretKills = None
+        self.totalMinionsKilled = None
+        self.champLevel = None
+        self.wardsKilled = None
+        
 
+    @classmethod 
+    def from_ParticipantStatsDto(self, ParticipantStatsDto): 
+        # print(ParticipantStatsDto)
+        player_serializer = PlayerSerializer() 
+        data = {}
+        for attr in player_serializer.__dict__.keys(): 
+            print(attr)
+            if attr in ParticipantStatsDto:
+                data[attr] = ParticipantStatsDto[attr]
+            elif attr in ParticipantStatsDto["status"]: 
+                data[attr] = ParticipantStatsDto["status"][attr]
+            else: 
+                raise Exception(f"Error : cannot retrieve attribute {attr} from the ParticipantStatsDto body.")
+        
+        return player_serializer(**data)
+
+    def to_dict(self): 
+        return self.__dict__
+
+
+# DONE
 class TeamSerializer: 
 
     def __init__(self): 
-        self.player1 = None # champ id and rank
+        self.player1 = None # champ 
         self.player2 = None 
         self.player3 = None 
         self.player4 = None 
@@ -106,35 +138,49 @@ class TeamSerializer:
         self.towerKills = None
     
         
-
     @classmethod
-    def from_dict(cls, team_response):
-        team_data = {
-            "teamId" : team_response["teamId"]
-            "status" : team_response["win"], 
-            "firstBlood" : team_response["firstBlood"],
-            "firstTower" : team_response["firstTower"], 
-            "firstBaron" : team_response["firstBaron"], 
-            "firstDragon" : team_response["firstDragon"], 
-            "towerKills" : team_response["towerKills"]
+    def from_dict(cls, TeamStatsDto, participantList):
+        '''
+            return a TeamSerializer instance 
+            Arguments : 
+                TeamStateDto : A single TeamStatsDto object, 1 team only, 
+                participantList : A list of all players in a team
+        '''
+        print(participantList)
+        data = {
+            "teamId" : TeamStatsDto["teamId"],
+            "status" : TeamStatsDto["win"], 
+            "firstBlood" : TeamStatsDto["firstBlood"],
+            "firstTower" : TeamStatsDto["firstTower"], 
+            "firstBaron" : TeamStatsDto["firstBaron"], 
+            "firstDragon" : TeamStatsDto["firstDragon"], 
+            "towerKills" : TeamStatsDto["towerKills"]
         }
 
-        
+        for idx, player in enumerate(participantList): 
+            player_obj = PlayerSerializer.from_ParticipantStatsDto(player)
+            data["player"+idx] = to_dict.to_dict()
+
+        serialized_team = TeamSerializer(**team_data) 
+        return serialized_team
+
+    def to_dict(self): 
+        return self.__dict__
 
 
 class MatchSerializer: 
     
-    def __init__(self, gameId, plateformId, gameDuration, seasonId, gameMode, teamOne, teamTwo, winner): 
-        self.gameId = gameId 
-        self.plateformId = plateformId 
-        self.gameDuration = gameDuration
-        self.seasonId = seasonId
-        self.gameMode = gameMode
+    def __init__(self): 
+        self.gameId = None 
+        self.platformId = None 
+        self.gameDuration = None
+        self.seasonId = None
+        self.gameMode = None
         # list of players in team 1
-        self.teamOne = teamOne 
+        self.teamOne = None 
         # list of players in team 2 
-        self.teamTwo = teamTwo
-        self.winner = winner
+        self.teamTwo = None
+        self.winner = None
 
 
     @classmethod 
@@ -146,18 +192,30 @@ class MatchSerializer:
         '''
         match_gen_details = {
             "gameId" : match_body["gameId"], 
-            "plateformeId" : match_body["plateformId"], 
+            "platformId" : match_body["platformId"], 
             "gameDuration" : match_body["gameDuration"], 
             "seasonId" : match_body["seasonId"], 
             "gameMode" : match_body["gameMode"]
         }
-        
-        match_instance = cls(**match_gen_details)
 
-        # build teams data 
-        tea
-        
+        match_obj = cls(**match_gen_details)
 
+        for i in range(0, 2): 
+            # get gen team data 
+            team_meta = match_body["team"]
+            team_id = team_meta["teamId"]
+            # get players from team 
+            players_list = [player for player in match_body["participants"] if player["teamId"] == team_id]
+            print(f"Player list size : {len(players_list)} \n")
+            #serializing 
+            team_serializer = TeamSerializer.from_dict()     
+            team_obj = team_serializer.to_dict()
+            if i == 0: 
+                match_obj.teamOne = team_obj
+            else: 
+                match_obj.teamTwo = team_obj
+        
+        return match_obj
 
     def to_csv(self, file_path): 
         # todo : format the instance as a csv
@@ -165,7 +223,10 @@ class MatchSerializer:
 
 
 user_id = fetch_user_account_id("oraxan")
+print(user_id)
 matches = fetch_user_matches(user_id)
 match_id = str(matches[0]["gameId"])
-print(match_id)
-fetch_match_details(match_id)
+match_resp = fetch_match_details(match_id)
+
+
+match_object = MatchSerializer.from_response_body(match_resp)
